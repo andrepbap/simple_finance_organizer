0e7e78688524dca8c3204cf88dc5f7ed812eac2f
developer <andre@MacBook-Air-de-Andre.local>
2023-06-19 23:04:31 -0300

    implementa botton navigation

--- lib/main.dart
+++ lib/main.dart
@@ -35,8 +35,14 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  TransactionVM? vm;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
   var isFirebaseReady = false;
+
+  int _selectedIndex = 0;
+  TransactionVM? vm;
+  late TransactionListView listView;
 
   HomePageState() {
     Future(() => _startFirebase());
@@ -48,10 +54,42 @@ class HomePageState extends State<HomePage> {
     );
 
     vm = TransactionVM();
+    listView = TransactionListView(vm: vm!);
 
     setState(() {
       isFirebaseReady = true;
     });
+  }
+
+  List<Widget> _getWidgetOptions() {
+    return <Widget>[
+      listView,
+      const Text(
+        'Index 1: Business',
+        style: optionStyle,
+      ),
+      const Text(
+        'Index 2: School',
+        style: optionStyle,
+      ),
+    ];
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _getScreenToNavigate() {
+    switch (_selectedIndex) {
+      case 0:
+        return CreateTransactionScreen(vm: vm!);
+      case 1:
+        return CreateTransactionScreen(vm: vm!);
+      default:
+        return CreateTransactionScreen(vm: vm!);
+    }
   }
 
   Widget _createContent() {
@@ -68,17 +106,36 @@ class HomePageState extends State<HomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Todas as Transações"),
       ),
-      body: TransactionListView(vm: vm!),
+      body: _getWidgetOptions().elementAt(_selectedIndex),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).push(
             MaterialPageRoute(
-              builder: (context) => CreateTransactionScreen(vm: vm!),
+              builder: (context) => _getScreenToNavigate(),
             ),
           );
         },
         tooltip: 'Add',
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: 'Transações',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Contas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[800],
+        onTap: _onItemTapped,
       ),
     );
   }
